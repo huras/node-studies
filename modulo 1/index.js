a3ff62bc0998847d1ce19e4d697da43d812bf5af
huras <hurast@gmail.com>
2019-08-02 12:24:38 -0300

    rota que retorna json

--- modulo 1/index.js
+++ modulo 1/index.js
@@ -11,4 +11,11 @@ app.get("/view/:id", (req, res) => {
   res.send(`Bem vindo id ${req.params.id}`);
 });
 
+//json return
+app.get("/json/:nome", (req, res) => {
+  return res.json({
+    message: `Welcome, ${req.params.nome}`
+  });
+});
+
 app.listen(3000);
